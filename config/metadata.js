23d16d8924bd69b54e839587b1177e479eea40a0
Martin Strutz <martinstrutz@users.noreply.github.com>
2020-05-20 14:45:46 -0400

    Update metadata

--- config/metadata.js
+++ config/metadata.js
@@ -1,8 +1,8 @@
 module.exports = {
-  title: 'The Coop',
+  title: 'HITW Test Landing Page',
   description: 'Farm fresh eggs from coop to stoop',
-  twitter: '@TheCoop',
-  keywords: ['The Coop', 'Chickens', 'Eggs'],
+  twitter: '@martinstrutz',
+  keywords: ['Clean', 'Eating', 'Healthy'],
   lang: 'en',
   image: {
     src: '/images/site.png', // Path to your image you placed in the 'static' folder
